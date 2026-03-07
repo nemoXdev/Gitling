b1f7f10e6b91c7ef569c4ea6dfc7f096d279c70a
Maneesha Cooray <bmaneeshacooray@gmail.com>
2026-03-08 00:13:44 +0530

    fix(deps): upgrade Kotlin to 2.1.10 to fix compiler crash

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 plugins {
     id("com.android.application") version "8.3.2" apply false
     id("com.android.library") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false  // Kotlin 2.0+ Compose compiler
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false  // Kotlin 2.0+ Compose compiler
 
     // Dependency updates plugin
     id("com.github.ben-manes.versions") version "0.53.0" apply false
