5d8ea0f6187d677fa4bdca29a4b0776894b317b6
Maneesha Cooray <bmaneeshacooray@gmail.com>
2026-06-21 18:22:48 +0530

    chore: bump version to 1.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.maneeshacooray.gitling"
         minSdk = 23
         targetSdk = 37
-        versionCode = 12
-        versionName = "1.0.11"
+        versionCode = 13
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
