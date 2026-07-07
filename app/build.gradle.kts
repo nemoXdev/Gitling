5c6848f588a39f4598d8efe0b99105f5576906e6
Maneesha Cooray <bmaneeshacooray@gmail.com>
2026-07-07 08:01:19 +0530

    chore: bump version to 1.0.48

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,8 +110,8 @@ android {
         applicationId = "com.maneeshacooray.gitling"
         minSdk = 23
         targetSdk = 37
-        versionCode = 47
-        versionName = "1.0.47"
+        versionCode = 48
+        versionName = "1.0.48"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
